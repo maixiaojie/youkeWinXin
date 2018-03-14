b45cf6ba5e61c063c47407adf725bdab9d267394
maixiaojie <778273234@qq.com>
2018-03-15 00:06:24 +0800

    微信signature

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 
+const token = "youkepingtai";
 /* GET home page. */
 router.get('/api/wechat', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const {signature, timestamp, nonce, echostr} = req.query;
+  if(!signature || !timestamp || !nonce || !echostr) {
+    return res.send("invalid request");
+  }
+  const params = [token, timestamp, nonce];
+  params.sort();
+  const str = params.join('');
+  const hash = crypto.createHash('sha1');
+  const sign = hash.update(str.digest('hex'));
+
+  if(sign === signature) {
+    res.send(echostr);
+  }else {
+    res.send('invalid sign');
+  }
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
